74bc90a24640db570e208b4cfd08573e4ebcc50f
Serhiy <serhiy.mos@gmail.com>
2023-12-22 15:52:35 +0400

    Test search/query params

--- src/app/docs/[[...id]]/page.tsx
+++ src/app/docs/[[...id]]/page.tsx
@@ -1,6 +1,11 @@
-const DocsPage = ({params}: any) => {
+interface PageProps {
+  params: { slug: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+};
+
+const DocsPage = ({ params, searchParams }: PageProps) => {
   return (
-    <div>Docs {JSON.stringify(params)}</div>
+    <div>Docs {JSON.stringify(params)} {JSON.stringify(searchParams)}</div>
   );
 }
 
