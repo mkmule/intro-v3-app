26eef5cf895ca0d29f0365197fe185ed179dfe0f
Serhiy <serhiy.mos@gmail.com>
2023-12-23 15:39:15 +0400

    Test css modules

--- src/app/docs/[[...id]]/page.tsx
+++ src/app/docs/[[...id]]/page.tsx
@@ -1,11 +1,13 @@
+import styles from './style.module.css';
+
 interface PageProps {
   params: { slug: string }
   searchParams: { [key: string]: string | string[] | undefined }
-};
+}
 
 const DocsPage = ({ params, searchParams }: PageProps) => {
   return (
-    <div>Docs {JSON.stringify(params)} {JSON.stringify(searchParams)}</div>
+    <div className={styles.title}>Docs {JSON.stringify(params)} {JSON.stringify(searchParams)}</div>
   );
 }
 
