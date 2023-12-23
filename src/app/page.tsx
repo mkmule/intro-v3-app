263b833a4d36201bb0d8292c424a1008a28f0537
Serhiy <serhiy.mos@gmail.com>
2023-12-23 16:04:06 +0400

    Added server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
-export default function Home() {
+const getData = async () => {
+  await new Promise<void>((res) => setTimeout(() => res(), 2000));
+  return { data: [1, 2, 3] };
+}
+
+// Server component example
+const Home = async () => {
+  const data = await getData();
+
   return (
-    <p>This is home</p>
+    <p>This is home: {JSON.stringify(data)}</p>
   )
 }
+
+export default Home
