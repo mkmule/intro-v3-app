578e39d44b541eed38ed8e70ee873b77be8552cd
Serhiy <serhiy.mos@gmail.com>
2023-12-31 14:25:41 +0300

    Test middleware

---
+++ src/middleware.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server';
+
+export const middleware = (request: Request) => {
+  return NextResponse.redirect(new URL('/', request.url))
+}
+
+export const config = {
+  matcher: ['/middle'],
+}
+
+export default middleware
